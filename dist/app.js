f03ca19976ffc1adffd5a66dbcadef75eb1d49f5
Brett Earle <brettearle@gmail.com>
2022-08-07 18:32:57 +0930

    added get by id and post routes to app.ts, ran build

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,16 @@ const express_1 = __importDefault(require("express"));
 const add_function_1 = __importDefault(require("./add_function"));
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    console.log((0, add_function_1.default)(5, 5));
-    res.send("Hello");
+    const addedNum = (0, add_function_1.default)(5, 5);
+    res.send(addedNum.toString());
+});
+app.post("/", (req, res) => {
+    res.send(`this is the post`);
+});
+app.get("/:id", (req, res) => {
+    const id = Number(req.query.id);
+    req.query.id
+        ? res.send(`this is the get by id route ${id}`)
+        : res.send("not found");
 });
 app.listen(5000, () => console.log("server running port 5000"));
--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send(addedNum.toString());
 });
 
+app.post("/", (req: Request, res: Response) => {
+  res.send(`this is the post`);
+});
+
+app.get("/:id", (req: Request, res: Response) => {
+  const id: number = Number(req.query.id);
+  req.query.id
+    ? res.send(`this is the get by id route ${id}`)
+    : res.send("not found");
+});
+
 app.listen(5000, () => console.log("server running port 5000"));
